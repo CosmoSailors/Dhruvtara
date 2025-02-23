ef8c1e2eee63b214bad927ff52bc23d595a52f59
Alaap Surendran <alaapsurendran@gmail.com>
2025-02-23 19:32:58 +0530

    alarm

--- app/src/main/java/com/grasstools/tangential/triggerables/AlarmTriggerable.kt
+++ app/src/main/java/com/grasstools/tangential/triggerables/AlarmTriggerable.kt
@@ -1,13 +1,22 @@
 package com.grasstools.tangential.triggerables
 
-import com.grasstools.tangential.triggerables.Triggerable
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.util.Log
+import androidx.core.content.ContextCompat.startActivity
+import com.grasstools.tangential.App
+import com.grasstools.tangential.presentation.ui.alarmscreen.AlarmScreen
 
 object AlarmTriggerable: Triggerable {
     override fun onEntry(config: String) {
-        TODO("Not yet implemented")
+        Log.i("LOL", "Hey?")
+        val intent = Intent(App.getContext()!!, AlarmScreen::class.java) // Replace NextActivity
+        intent.setFlags(FLAG_ACTIVITY_NEW_TASK + FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(App.getContext()!!, intent, null)
     }
 
     override fun onExit(config: String) {
-        TODO("Not yet implemented")
+        // TODO("Not yet implemented")
     }
 }
